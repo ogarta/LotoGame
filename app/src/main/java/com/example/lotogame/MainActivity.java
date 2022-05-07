5e1059a8544c7e64285a9dfc26da37aa53efc0f6
ogata-higan <lviet456@gmail.com>
2022-05-07 14:00:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lotogame/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.lotogame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    ScrollView scrollView;
+    TextView txtNumRao, txtNumRao_pre;
+    Button btnReset, btnRao;
+    ArrayList<Integer> num = new ArrayList<Integer>();
+    Random random = new Random();
+    String arrayNum = "";
+    int number;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        scrollView = findViewById(R.id.scrollView);
+        txtNumRao = findViewById(R.id.textViewSoRao);
+        txtNumRao_pre = findViewById(R.id.textViewHienThiSoDaRao);
+        btnRao = findViewById(R.id.buttonRao);
+        btnReset = findViewById(R.id.buttonReset);
+
+        create_ArrayListNum();
+
+        btnRao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(num.size() == 0){
+                    Toast.makeText(MainActivity.this,"Da het so",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                number = random.nextInt(num.size());
+                txtNumRao.setText(num.get(number).toString());
+
+                arrayNum += num.get(number).toString() +" - ";
+                txtNumRao_pre.setText(arrayNum);
+                num.remove(number);
+                scrollView.fullScroll(View.FOCUS_DOWN);
+            }
+        });
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                create_ArrayListNum();
+                txtNumRao.setText("");
+                arrayNum = "";
+                txtNumRao_pre.setText(arrayNum);
+            }
+        });
+
+    }
+    public void create_ArrayListNum(){
+        num.clear();
+        for (int i = 1; i <= 100; i++) {
+            num.add(i);
+        }
+    }
+}
